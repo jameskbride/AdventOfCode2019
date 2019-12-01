21c245cc2bded367bc6a92fa572fc2aadb111cf0
Jim Kirkbride <james.kirkbride@gmail.com>
2019-12-01 07:52:51 -0500

    Initial Commit

---
+++ AdventOfCode/AdventOfCode.swift
@@ -0,0 +1,11 @@
+//
+//  AdventOfCode.swift
+//  AdventOfCode
+//
+//  Created by James Kirkbride on 12/1/19.
+//  Copyright © 2019 James Kirkbride. All rights reserved.
+//
+
+class AdventOfCode {
+
+}
